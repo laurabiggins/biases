c1b97a7f8b7024ac2d82a223b567ccc792fd02c9
laurabiggins <laurabiggins10@gmail.com>
2017-09-25 13:49:44 +0100

    updated file paths in run_htseq.sh

--- processing/experimental_data/run_htseq.sh
+++ processing/experimental_data/run_htseq.sh
@@ -14,11 +14,13 @@ do
 	# samtools doesn't seem to get rid of the temp files unless it creates a merged version so I'm writing the file out. We can clean up afterwards.
 	samtools sort -O BAM -o $SORTED$FILE $FILE
 	
+	GTF_FILE='/bi/group/bioinf/Laura_B/bias_analysis/gtf_files/mouse/Mus_musculus.GRCm38.88.gtf.gz'
+	
 	# get the counts
-	htseq-count --format bam --stranded=reverse $SORTED$FILE /bi/group/bioinf/Laura_B/bias_analysis/GRCm38_loci/Mus_musculus.GRCm38.88.gtf.gz > $FILE$HT_OUTPUT;
+	htseq-count --format bam --stranded=reverse $SORTED$FILE GTF_FILE > $FILE$HT_OUTPUT;
 	
 	# add in the gene names
-	perl /bi/group/bioinf/Laura_B/bias_analysis/TIDIED/scripts/get_genesymbols_from_ensembl_ids.pl --gtf /bi/group/bioinf/Laura_B/bias_analysis/GRCm38_loci/Mus_musculus.GRCm38.88.gtf.gz --file $FILE$HT_OUTPUT;
+	perl /bi/group/bioinf/Laura_B/bias_analysis/git/biases/processing/experimental_data/get_genesymbols_from_ensembl_ids.pl --gtf GTF_FILE --file $FILE$HT_OUTPUT;
 	
 done
 
